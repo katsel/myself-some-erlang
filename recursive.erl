18199e84dfd5c87cfafcbecaae9c14ec6e7a3c02
katsel <katsel@users.noreply.github.com>
2016-05-13 14:35:26 +0200

    add/improve comments

--- recursive.erl
+++ recursive.erl
@@ -4,30 +4,34 @@
         tail_sublist_2/2, zip/2, lenient_zip/2, noomit_zip/2, quicksort/1,
         lc_quicksort/1]).
 
+%% basic recursive factorial function
 fac(N) when N == 0 -> 1;
 fac(N) when N >  0 -> N * fac(N-1).
 
+%% find the length of a list
 len([]) -> 0;
 len([_|T]) -> 1 + len(T).
 
-%% tail recursion
+%% tail recursive version of fac/1
 tail_fac(N) -> tail_fac(N, 1).
 
 tail_fac(0, Acc) -> Acc;
 %% why not: tail_fac(N, Acc) -> tail_fac(N-1, N*Acc).
 tail_fac(N, Acc) when N > 0 -> tail_fac(N-1, N*Acc).
 
+%% tail recursive version of len/1
 tail_len(L) -> tail_len(L, 0).
 
 tail_len([], Acc) -> Acc;
 tail_len([_|L], Acc) -> tail_len(L, 1 + Acc).
 
-%% create list of N identical Term elements
+%% duplicates Term N times
 duplicate(0, _) ->
     [];
 duplicate(N, Term) when N > 0 ->
     [Term | duplicate(N-1, Term)].
 
+%% tail recursive version of duplicate/1
 tail_duplicate(N, Term) ->
     tail_duplicate(N, Term, []).
 
@@ -40,6 +44,7 @@ tail_duplicate(N, Term, List) when N > 0 ->
 reverse([]) -> [];
 reverse([H|T]) -> reverse(T) ++ [H].
 
+%% tail recursive version of reverse/1
 tail_reverse(List) ->
     tail_reverse(List, []).
 
@@ -51,6 +56,7 @@ sublist(_, 0) -> [];
 sublist([], _) -> [];
 sublist([H|T], N) when N > 0 -> [H|sublist(T, N-1)].
 
+%% tail recursive version of sublist/2
 tail_sublist(List, N) -> reverse(tail_sublist(List, N, [])).
 
 tail_sublist(_, 0, SubList) -> SubList;
@@ -66,21 +72,30 @@ tail_sublist_2([], _, SubList) -> SubList;
 tail_sublist_2([Head|Tail], N, SubList) when N > 0 ->
     tail_sublist_2(Tail, N-1, SubList ++ [Head]).
 
+%% Take two lists [A] and [B] and return a list of tuples
+%% with the form [{A, B}]. Both lists need to be of the same length.
 zip([], []) -> [];
 zip([X|Xs], [Y|Ys]) -> [{X,Y}|zip(Xs, Ys)].
 
-% handles list with different length (omit additional elements)
+%% same as zip/2, but lists can vary in length (omit additional elements)
 lenient_zip([],_) -> [];
 lenient_zip(_,[]) -> [];
 lenient_zip([X|Xs], [Y|Ys]) -> [{X,Y}|lenient_zip(Xs, Ys)].
 
-% handles lists with different length (fill missing elements with empty list)
+%% same as zip/2, but lists can vary in length (fill missing elements with
+%% an empty list)
 noomit_zip([], []) -> [];
 noomit_zip([], [Y|Ys]) -> [{[],Y}|noomit_zip([], Ys)];
 noomit_zip([X|Xs], []) -> [{X,[]}|noomit_zip(Xs, [])];
 noomit_zip([X|Xs], [Y|Ys]) -> [{X,Y}|noomit_zip(Xs, Ys)].
 
+%% tail recursive version of zip/2
 
+%% tail recursive version of lenient_zip/2
+
+%% tail recursive version of noomit_zip/2
+
+%% basic quicksort function
 quicksort([]) -> [];
 quicksort([Pivot|Rest]) ->
     {Smaller, Larger} = partition(Pivot, Rest, [], []),
@@ -92,7 +107,7 @@ partition(Pivot, [H|T], Smaller, Larger) ->
        H >  Pivot -> partition(Pivot, T, Smaller, [H|Larger])
     end.
 
-% quicksort with list comprehensions
+% quicksort with list comprehensions rather than a partition function
 lc_quicksort([]) -> [];
 lc_quicksort([Pivot|Rest]) ->
     lc_quicksort([Smaller || Smaller <- Rest, Smaller =< Pivot])
