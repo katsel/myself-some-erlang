b80def7bd4b0fec15fcda3f9f216aeaa1b71446d
katsel <katsel@users.noreply.github.com>
2016-05-29 14:42:36 +0200

    Higher order functions: Add filter funct, fold funct, console output

--- hhfuns.erl
+++ hhfuns.erl
@@ -42,3 +42,56 @@ old_men([], Acc) -> Acc;
 old_men([Person = {male, Age}|People], Acc) when Age > 60 ->
     old_men(People, [Person|Acc]);
 old_men([_|People], Acc) -> old_men(People, Acc).
+
+
+%% Maps, filters, folds and more
+
+filter(Pred, L) -> lists:reverse(filter(Pred, L, [])).
+
+filter(_, [], Acc) -> Acc;
+filter(Pred, [H|T], Acc) ->
+    case Pred(H) of
+        true -> filter(Pred, T, [H|Acc]);
+        false -> filter(Pred, T, Acc)
+    end.
+
+%% -> Folds
+
+%% find the maximum of a list
+max([H|T]) -> max2(T, H).
+
+max2([], Max) -> Max;
+max2([H|T], Max) when H > Max -> max2(T, H);
+max2([_|T], Max) -> max2(T, Max).
+
+%% find the minimum of a list
+min([H|T]) -> min2(T, H).
+
+min2([], Min) -> Min;
+min2([H|T], Min) when H < Min -> min2(T, H);
+min2([_|T], Min) -> min2(T, Min).
+
+%% sum all the elements in a list
+sum(L) -> sum(L, 0).
+
+sum([], Sum) -> Sum;
+sum([H|T], Sum) -> sum(T, H+Sum).
+
+fold(_, Start, []) -> Start;
+fold(F, Start, [H|T]) -> fold(F, F(H,Start), T).
+
+%% reverse, map, filter as folds
+reverse_fold(L) ->
+    fold(fun(X,Acc) -> [X|Acc] end, [], L).
+
+map_fold(F, L) ->
+    reverse_fold(fold(fun(X, Acc) -> [F(X)|Acc] end, [], L)).
+
+filter_fold(Pred, L) ->
+    F = fun(X, Acc) ->
+        case Pred(X) of
+            true -> [X|Acc];
+            false -> Acc
+        end
+    end,
+    reverse_fold(fold(F, [], L)).
